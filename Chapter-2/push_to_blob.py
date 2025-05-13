925581584f490e345d993e3b919f9265f6402528
Aurelius <aurelius.oliveira@gmail.com>
2025-05-12 21:00:27 -0300

    Update connection string variable name and add requirements file

--- Chapter-2/push_to_blob.py
+++ Chapter-2/push_to_blob.py
@@ -39,7 +39,7 @@ def to_blob(func):
     parquet_buffer = BytesIO()
     pq.write_table(table, parquet_buffer)
 
-    connection_string = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
+    connection_string = os.getenv('BLOB_CONN_STRING')
     blob_service_client = BlobServiceClient.from_connection_string(connection_string)
 
     container_name = "testtech"
